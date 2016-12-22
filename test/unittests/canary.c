aeb61cf33244cea9db7cab7d07ab9b1d31bf5d50
Douglas Moore <doug@dglmoore.com>
2016-12-22 12:16:41 -0700

    Setup unit testing

---
+++ test/unittests/canary.c
@@ -0,0 +1,13 @@
+// Copyright 2016 Douglas Moore. All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+#include <unit.h>
+
+UNIT(OnePlusTwo)
+{
+  ASSERT_EQUAL(3, 1+2);
+}
+
+BEGIN_SUITE(Canary)
+  ADD_UNIT(OnePlusTwo)
+END_SUITE
---
+++ test/unittests/main.c
@@ -0,0 +1,13 @@
+// Copyright 2016 Douglas Moore. All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+#include <unit.h>
+
+IMPORT_SUITE(Canary);
+IMPORT_SUITE(Vector);
+
+BEGIN_REGISTRATION
+  REGISTER(Canary)
+END_REGISTRATION
+
+UNIT_MAIN();
