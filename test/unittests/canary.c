eaf32cc7a1629c751608d61f1204ca9d31e76179
Douglas Moore <doug@dglmoore.com>
2017-10-11 22:27:50 -0700

    Update copyright headers

--- test/unittests/canary.c
+++ test/unittests/canary.c
@@ -1,4 +1,4 @@
-// Copyright 2016 Douglas Moore. All rights reserved.
+// Copyright 2016-2017 Douglas G. Moore. All rights reserved.
 // Use of this source code is governed by a MIT
 // license that can be found in the LICENSE file.
 #include <ginger/unit.h>
