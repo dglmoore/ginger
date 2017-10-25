9b5dad9a4afd7138a9b28f40c1af35af88654232
Douglas Moore <doug@dglmoore.com>
2017-10-24 22:09:38 -0700

    Setup examples

---
+++ examples/vector/alloc.c
@@ -0,0 +1,19 @@
+#include <ginger/vector.h>
+#include <stdio.h>
+
+int main()
+{
+    int *xs = gvector_alloc(5, 3, sizeof(int));
+    if (xs)
+    {
+        printf("Capacity:     %d\n", (int)gvector_cap(xs));
+        printf("Length:       %d\n", (int)gvector_len(xs));
+        printf("Element Size: %d\n", (int)gvector_size(xs));
+        for (size_t i = 0; i < gvector_len(xs); ++i)
+        {
+            printf("%d ", xs[i]);
+        }
+        printf("\n");
+    }
+    gvector_free(xs);
+}
